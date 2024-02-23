60e2c4a4be5e662abde79db05f490c804cbf4e7c
Danilo Leocata <danilo.leocata@gmail.com>
2024-02-23 17:42:52 +0100

    Updates

--- _app/immutable/chunks/paths.wQ6WnuUe.js
+++
@@ -1,1 +0,0 @@
-var s;const e=((s=globalThis.__sveltekit_1tcjt6j)==null?void 0:s.base)??"/sveltekit-fe";var t;const a=((t=globalThis.__sveltekit_1tcjt6j)==null?void 0:t.assets)??e;export{a,e as b};
---
+++ _app/immutable/entry/start.YzideTT2.js
@@ -0,0 +1,1 @@
+import{a as t}from"../chunks/entry.COCrkNvq.js";export{t as start};
