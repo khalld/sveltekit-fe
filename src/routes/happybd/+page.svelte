b344b6b59fbb8f3ef24060f789fcec9293c1b4b2
Danilo Leocata <37438774+khalld@users.noreply.github.com>
2025-04-16 17:26:24 +0200

    ripristinata happybd

--- src/routes/happybd/+page.svelte
+++ src/routes/happybd/+page.svelte
@@ -7,7 +7,7 @@
     Benvenuta utente!
 </h1>
 
-<div class="continaer">
+<div class="container">
     <div class="row">
         <div class="col-12 text-center">
             <img src="https://media.tenor.com/FJonHWl-xVsAAAAM/ketelaere-de-ketelaere.gif" alt="Happy Birthday">
@@ -18,7 +18,7 @@
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae nisi nemo at officiis illo, quaerat numquam facilis possimus adipisci. Odio dolorem sapiente dolorum deleniti sit doloribus dolores molestias architecto aliquam!
         </p>
         <div class="w-100"></div>
-        <a class="col col-12 btn btn-primary px-4" href="{base}/happybd/1">Happy Birthday</a>
+        <a class="col col-12 btn btn-lg btn-primary mx-4" href="{base}/happybd/1">Happy Birthday</a>
 
     </div>
 </div>
--- src/routes/happybd/present/+page.svelte
+++ src/routes/happybd/present/+page.svelte
@@ -1,1 +1,43 @@
-<h1>happy present!</h1>
+<script>
+    import Heart from '../../../lib/Heart.svelte';
+    import ControlsBirthday from '../../../lib/player/ControlsBirthday.svelte'
+
+    let innerWidth = 2000;
+    let innerHeight = 1000;
+
+    let hearts = [];
+
+    function generateHearts() {
+        for (let i = 0; i < 100; i++) {
+        hearts.push({
+            id: i,
+            topPos: Math.random() * innerHeight,
+            leftPos: Math.random() * innerWidth
+        });
+        }
+    }
+    generateHearts();
+
+</script>
+
+
+<svelte:window bind:innerWidth bind:innerHeight />
+
+{#each hearts as { id, topPos, leftPos }}
+    <Heart {topPos} {leftPos} />
+{/each}
+
+<div class="container">
+    <h1>happy present!</h1>
+    <div class="row">
+        <div class="col-12 text-center">
+            <img src="https://media.tenor.com/FJonHWl-xVsAAAAM/ketelaere-de-ketelaere.gif" alt="Happy Birthday">
+        </div>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia, voluptatum dignissimos, error totam sunt iste ea non architecto omnis sapiente atque placeat eveniet aliquam, cupiditate saepe ipsam! Rerum, aliquam labore!</p>
+    </div>
+</div>
+
+
+<div class="container w-50 mt-3 mb-4">
+    <ControlsBirthday/>
+</div>
